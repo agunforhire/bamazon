d54c06849fd8c72a9689e8cebebe73bf6e6ab1eb
James Wilcox <architeuthisdux@protonmail.com>
2018-12-12 23:55:22 -0800

    Final Commit

---
+++ app.js
@@ -0,0 +1,95 @@
+let mysql = require('mysql');
+let inquirer = require('inquirer');
+let Table = require('easy-table');
+let chalk = require('chalk');
+let keys = require('./keys');
+let sqlpass = keys.SQLpw;
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: sqlpass,
+    database: "bamazon"
+});
+let troveArray = [];
+
+displayLoot();
+
+function connectToDB() {
+    connection.connect(function(err) {
+        if (err) throw err;
+		console.log("connected as id " + connection.threadId);
+    });
+}
+
+function disconnectDB() {
+	connection.end();
+	console.log('Goodbye!');
+}
+
+function displayLoot() {
+	connectToDB();
+	console.log('*.*.* Welcome to Gilmore\'s Glorious Goods! *.*.*');
+    connection.query('SELECT * FROM trove', function(err, res, fields) {  
+        if (err) throw err;
+		else if (res.length > 0) {
+			res.forEach((loot) => troveArray.push(loot) );
+			console.log('got products...');
+			var lootTable = new Table;
+			troveArray.forEach( (product) => {
+				lootTable.cell('Id', product.id, Table.number(0));
+                lootTable.cell('Description', product.product_name);
+                lootTable.cell('Department', product.department_name);
+                lootTable.cell('Price - in Gold', product.price, Table.number(3));
+                lootTable.cell('Stock', product.stock_ea, Table.number(4));
+				lootTable.newRow();
+			});
+			console.log(lootTable.toString());
+			promptToBuyProduct();
+		}
+        else console.log('Sold out of all items! Please come back soon.');
+	});
+	
+}
+
+function promptToBuyProduct() {
+	let idToPurchase, itemToPurchase, maxQuantity, price, quantity;
+	inquirer.prompt([{
+		name: 'id',
+		type: 'input',
+		message: 'Which product would you like to buy?',
+		validate: (itemId) => {
+			return (!isNaN(itemId) && itemId > 0 && itemId <= troveArray.length);
+		}
+	}]).then( (purchase) => {
+		idToPurchase = purchase.id;
+		itemToPurchase = troveArray[purchase.id - 1].product_name;
+		maxQuantity = parseInt(troveArray[purchase.id - 1].stock_ea);
+		price = parseFloat(troveArray[purchase.id - 1].price).toFixed(2);
+		inquirer.prompt([{
+			name: 'quantity',
+			type: 'input',
+			message: 'How many would you like to buy',
+			validate: (quantity) => {
+				return (!isNaN(quantity) && quantity > 0 && quantity <= maxQuantity);
+			}
+		}]).then( (purchase) => {
+			const salesTotal = parseInt(purchase.quantity) * price;
+			quantity = purchase.quantity;
+			console.log(`Ok, purchasing ${quantity} of ${itemToPurchase} for $ ${salesTotal}.`);
+			purchaseItem(idToPurchase, quantity);
+		});
+	});
+}
+
+function purchaseItem(productId, purchaseQuantity) {
+	let query = connection.query(
+		"UPDATE trove SET ? WHERE ?",
+		[{ stock_ea: purchaseQuantity }, { id: productId }],
+		function(err, res) {
+			console.log(res.affectedRows + " products updated!\n");
+		});
+	console.log(query.sql);
+	disconnectDB();
+}
---
+++ bamazon.sql
@@ -0,0 +1,30 @@
+DROP DATABASE IF EXISTS bamazon;
+CREATE database bamazon;
+USE bamazon;
+
+CREATE TABLE trove (
+ id INT AUTO_INCREMENT NOT NULL,
+ product_name VARCHAR(75) NOT NULL,
+ department_name VARCHAR(30) NOT NULL,
+ price DECIMAL(10,4) NOT NULL,
+ stock_ea INT(10) NOT NULL,
+ PRIMARY KEY (id)
+);
+
+INSERT INTO trove (product_name,department_name,price,stock_ea)
+VALUES ("Vorpal Blade","Weapons",999.99,3),
+       ("Ars Geotia: First Edition","Books", 1499.99,10),
+       ("A hut on hens legs", "Transportation", 4999.99, 2),
+       ("Bootleg Ice Cream", "Food & Drink", 99.99, 199),
+       ("Abandoned Clown Shoe", "Clothing", 24.99, 99),
+       ("Snips, Snails, and Puppydog Tails", "Sundries", 0.99, 999),
+       ("Sugar, Spice, and Everything Nice", "Sundries", 0.99, 999),
+       ("The Lesser Key of Solomon", "Books", 199.99, 20),
+       ("A Flying Carpet", "Transportation", 1999.99, 5),
+       ("A Black Lotus", "Sundries", 49999, 1),
+       ("A Living Gingerbread Man", "Food & Drink", 29.99, 499),
+       ("The Spice Melange", "Food & Drink", 9999.99, 15 ),
+       ("The BFG 9000", "Weapons", 4999.99, 10);
+
+    
+SELECT * FROM bamazon.trove;
